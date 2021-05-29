a5824aa9fea92bc48de0cc64813d4b21100405ec
Mara <alaneraerts@live.be>
2021-05-29 17:59:52 +0200

    cards

--- components/blog/Page.jsx
+++ components/blog/Page.jsx
@@ -1,18 +1,19 @@
 import React, {useEffect, useState} from 'react';
-import Cards from "./Cards";
+import Cards from "../cards/Cards";
 import Header from "../layouts/header/Header";
+import styles from "./Page.module.scss";
 
 function Page({blogs}) {
 
     return (
-        <>
+        <div style={{backgroundColor: 'silver'}}>
             <Header screen={true} color={"red"} height={"400px"}/>
-            {blogs.map(b => (
-                <>
+            <div className={styles.blog_container + " wrapper"}>
+                {blogs.map(b => (
                     <Cards blogs={b}/>
-                </>
-            ))}
-        </>
+                ))}
+            </div>
+        </div>
     );
 }
 
---
+++ components/cards/Cards.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './Cards.module.scss';
+
+function Cards({blogs}) {
+
+    return (
+        <article>
+            <a>
+                <div className={styles.cards}>
+                    <div className={styles.card_image}>
+                        <img src="/images/game.png" alt=""/>
+                    </div>
+                    <div className={styles.card_content}>
+                        <h4>{blogs.title}</h4>
+                        <p>{blogs.title}</p>
+                    </div>
+                </div>
+            </a>
+        </article>
+    );
+}
+
+export default Cards;
