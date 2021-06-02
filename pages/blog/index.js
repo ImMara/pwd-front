11b6761b07750710572c01aaef7e5054accbfe01
Mara <alaneraerts@live.be>
2021-06-02 02:55:06 +0200

    bg s3 c

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -18,7 +18,9 @@ function Index(props) {
                     height:'400px',
                     top:0,
                     left:0,
-                    background: 'linear-gradient(to top, rgba(241,241,243,1) 0%, rgba(0,0,0,0) 100%)'}}/>
+                    background: 'linear-gradient(to top, rgba(241,241,243,1) 0%, rgba(0,0,0,0) 100%)'
+                    }}
+                />
                 <img src="/images/header.jpg" style={{height:'100%', width:'100%', objectFit:'cover',}} alt=""/>
             </Header>
             <Page blogs={props.blogs}/>
