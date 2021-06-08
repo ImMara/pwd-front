81aea0beab3f5d67abaaffa72849bc7c6d68c0b3
Mara <alaneraerts@live.be>
2021-06-08 04:39:02 +0200

    anim

--- components/slides/one/First.jsx
+++ components/slides/one/First.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import Image from 'next/image';
 import styles from './First.module.scss';
 import Link from 'next/link';
@@ -7,8 +7,28 @@ function First(props) {
 
     const { children } = props;
 
+    const img = useRef()
+    const img2 = useRef()
+    const round = useRef()
+
+    const [mouseX,setMouseX] = useState();
+    const [mouseY,setMouseY] = useState();
+
+    const mousePosition = (event) =>{
+        setMouseX(event.clientX)
+        setMouseY(event.clientY)
+
+        console.log(img.current)
+        img.current.style.top = 42+(mouseY * 0.002)+"%"
+        img.current.style.left = 53+(mouseX * 0.002)+"%"
+
+        img2.current.style.top = 80-(mouseY * 0.0035)+"%"
+        img2.current.style.left = 50-(mouseX * 0.0035)+"%"
+
+    }
+
     return (
-        <div className={styles.container}>
+        <div className={styles.container} onMouseMove={mousePosition}>
             <div className={"wrapper "+styles.content}>
                 <div className={styles.c}>
                     <h1>ENERGY DRINK</h1>
@@ -27,9 +47,9 @@ function First(props) {
                     </div>
                 </div>
                 <div className={styles.c2}>
-                    <div className={styles.round}>
-                        <img src="/images/svg/Group.svg" alt="energy-can"/>
-                        <img src="/images/svg/plan.svg" alt="gaming-setup"/>
+                    <div className={styles.round} ref={round}>
+                        <img ref={img} src="/images/svg/Group.svg" alt="energy-can"/>
+                        <img ref={img2} src="/images/svg/plan.svg" alt="gaming-setup"/>
                     </div>
                 </div>
             </div>
